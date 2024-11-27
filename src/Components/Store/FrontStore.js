0acb06fba2436d29b4525a0bb85591c3ea515290
tortangChris <eliseriochrisashley@gmail.com>
2024-11-27 19:45:15 +0800

    Added SideNavigationStore component to FrontStore parent component

--- src/Components/Store/FrontStore.js
+++ src/Components/Store/FrontStore.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import SideNavigationStore from '../Side Navigation/SideNavigationStore';
 
 const FrontStore = () => {
+
   return (
-    <div>
-      
+    <div className="d-flex">
+      <SideNavigationStore />
     </div>
-  )
-}
+  );
+};
 
 export default FrontStore
