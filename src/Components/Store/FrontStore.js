727762f66754acabf0e48a5731c7ae996e272068
tortangChris <eliseriochrisashley@gmail.com>
2024-11-27 19:50:23 +0800

    Added Product List component to FrontStore parent component

--- src/Components/Store/FrontStore.js
+++ src/Components/Store/FrontStore.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import SideNavigationStore from '../Side Navigation/SideNavigationStore';
+import ProductList from './ProductList';
 
 const FrontStore = () => {
 
   return (
     <div className="d-flex">
       <SideNavigationStore />
+      <div>
+        <ProductList />
+      </div>
     </div>
   );
 };
