73fcb35aac393a4f5213a24d9c2c17a574fd499b
tortangChris <eliseriochrisashley@gmail.com>
2024-11-27 21:21:58 +0800

    Added Search component to FrontStore for product filtering

--- src/Components/Store/FrontStore.js
+++ src/Components/Store/FrontStore.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideNavigationStore from '../Side Navigation/SideNavigationStore';
 import ProductList from './ProductList';
+import Search from '../AdminInventory/Search';
 
 const FrontStore = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
 
   return (
     <div className="d-flex">
       <SideNavigationStore />
+
       <div style={{ flex: 1, marginLeft: '60px', padding: '10px', }}>
-        <ProductList />
+        <div className="d-flex justify-content-center my-3">
+          <Search onSearch={handleSearch} />
+        </div>
+
+        <ProductList searchTerm={searchTerm} />
       </div>
     </div>
   );
