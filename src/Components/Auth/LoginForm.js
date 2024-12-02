cfe91b2c8c28b35d68cfa746254cc88ee5c3db38
tortangChris <eliseriochrisashley@gmail.com>
2024-12-02 19:50:03 +0800

    Change Color Theme

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,66 +1,71 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import AuthService from '../../Services/AuthService'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import AuthService from "../../Services/AuthService";
+import { useNavigate } from "react-router-dom";
 
-const LoginForm = ({ triggerAlert, onLogin }) => { 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+const LoginForm = ({ triggerAlert, onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const userData = await AuthService.login(email, password);
-            if (userData) {
-                onLogin(); 
-                navigate('/dashboard');
-            }
-        } catch (error) {
-            triggerAlert('Invalid email or password.'); 
-        }
-    };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = await AuthService.login(email, password);
+      if (userData) {
+        onLogin();
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      triggerAlert("Invalid email or password.");
+    }
+  };
 
-    return (
-        <Form onSubmit={handleLogin}>
-            <Form.Group controlId="formEmail">
-                <Form.Control 
-                    type="email" 
-                    placeholder="Email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+  return (
+    <Form onSubmit={handleLogin}>
+      <Form.Group controlId="formEmail">
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            <Form.Group controlId="formPassword">
-                <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                <Button
-                    type="submit"
-                    style={{ 
-                        backgroundColor: '#f0a42c', 
-                        borderColor: '#f0a42c', 
-                        color: 'black',
-                        padding: '10px 20px' 
-                    }}
-                >
-                    Login
-                </Button>
-            </div>
-
-        </Form>
-    );
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        <Button
+          type="submit"
+          style={{
+            backgroundColor: "#FFD700",
+            borderColor: "#FFD700",
+            color: "black",
+            padding: "10px 20px",
+          }}
+        >
+          Login
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default LoginForm;
--- src/Components/Auth/SignUpForm.js
+++ src/Components/Auth/SignUpForm.js
@@ -1,112 +1,117 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import AuthService from '../../Services/AuthService'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import AuthService from "../../Services/AuthService";
 
 const SignUpForm = ({ triggerAlert }) => {
-    const [userDetails, setUserDetails] = useState({
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setUserDetails({ ...userDetails, [name]: value });
-        setError('');
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails({ ...userDetails, [name]: value });
+    setError("");
+  };
 
-    const handleSignup = async (e) => {
-        e.preventDefault();
-        
-        if (userDetails.password !== userDetails.confirmPassword) {
-            setError('Passwords do not match. Please try again.');
-            return;
-        }
+  const handleSignup = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await AuthService.signup(userDetails);
-            if (response) {
-            }
-        } catch (error) {
-            if (error.response && error.response.status === 422) {
-                triggerAlert('Email already exists.');
-            } else {
-                triggerAlert('Network error. Please check your connection.');
-            }
-        }
-    };
+    if (userDetails.password !== userDetails.confirmPassword) {
+      setError("Passwords do not match. Please try again.");
+      return;
+    }
 
-    return (
-        <Form onSubmit={handleSignup}>
-            <Form.Group controlId="formName">
-                <Form.Control 
-                    type="text" 
-                    placeholder="Name" 
-                    name="name" 
-                    value={userDetails.name} 
-                    onChange={handleInputChange} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+    try {
+      const response = await AuthService.signup(userDetails);
+      if (response) {
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        triggerAlert("Email already exists.");
+      } else {
+        triggerAlert("Network error. Please check your connection.");
+      }
+    }
+  };
 
-            <Form.Group controlId="formEmail">
-                <Form.Control 
-                    type="email" 
-                    placeholder="Email" 
-                    name="email" 
-                    value={userDetails.email} 
-                    onChange={handleInputChange} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+  return (
+    <Form onSubmit={handleSignup}>
+      <Form.Group controlId="formName">
+        <Form.Control
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={userDetails.name}
+          onChange={handleInputChange}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            <Form.Group controlId="formPassword">
-                <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    name="password" 
-                    value={userDetails.password} 
-                    onChange={handleInputChange} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+      <Form.Group controlId="formEmail">
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={userDetails.email}
+          onChange={handleInputChange}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            <Form.Group controlId="formConfirmPassword">
-                <Form.Control 
-                    type="password" 
-                    placeholder="Confirm Password" 
-                    name="confirmPassword" 
-                    value={userDetails.confirmPassword} 
-                    onChange={handleInputChange} 
-                    required 
-                    className="mb-3"
-                />
-            </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={userDetails.password}
+          onChange={handleInputChange}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            {error && <div className="text-danger mb-3">{error}</div>}
+      <Form.Group controlId="formConfirmPassword">
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          value={userDetails.confirmPassword}
+          onChange={handleInputChange}
+          required
+          className="mb-3"
+        />
+      </Form.Group>
 
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                <Button
-                    type="submit"
-                    style={{ 
-                        backgroundColor: '#f0a42c', 
-                        borderColor: '#f0a42c', 
-                        color: 'black',
-                        padding: '10px 20px' 
-                    }}
-                >
-                    SignUp
-                </Button>
-            </div>
+      {error && <div className="text-danger mb-3">{error}</div>}
 
-        </Form>
-    );
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        <Button
+          type="submit"
+          style={{
+            backgroundColor: "#FFD700",
+            borderColor: "#FFD700",
+            color: "black",
+            padding: "10px 20px",
+          }}
+        >
+          SignUp
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default SignUpForm;
