0f344c02db8579c7face7c7cb42d66b326664700
tortangChris <eliseriochrisashley@gmail.com>
2024-12-02 19:24:06 +0800

    Removed localStorage dependency

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,38 +1,40 @@
-import Api from './Api';
+import Api from "./Api";
 
 const AuthService = {
-    login: async (email, password) => {
-        try {
-            const response = await Api.post('/login', { email, password });
-            if (response.data.token) {
-                localStorage.setItem('user', JSON.stringify(response.data));
-            }
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    signup: async (userDetails) => {
-        try {
-            const response = await Api.post('/signup', userDetails);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    logout: () => {
-        localStorage.removeItem('user');
-    },
-
-    getCurrentUser: () => {
-        return JSON.parse(localStorage.getItem('user'));
-    },
-
-    isAuthenticated: () => {
-        return !!localStorage.getItem('user');
+  login: async (email, password) => {
+    try {
+      const response = await Api.post("/login", { email, password });
+      return response.data;
+    } catch (error) {
+      throw error;
     }
+  },
+
+  signup: async (userDetails) => {
+    try {
+      const response = await Api.post("/signup", userDetails);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  logout: async () => {
+    try {
+      await Api.post("/logout");
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  isAuthenticated: async () => {
+    try {
+      const response = await Api.get("/isAuthenticated");
+      return response.data.isAuthenticated;
+    } catch (error) {
+      return false;
+    }
+  },
 };
 
 export default AuthService;
